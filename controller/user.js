39539285c952650c7a2f8c0bb76860b9224ae4ed
テラリウム <terrarium@MacBook.local>
2017-10-26 22:43:14 +0900

    user

---
+++ controller/user.js
@@ -0,0 +1,7 @@
+const log4js = require('log4js');
+const logger = log4js.getLogger();
+logger.level = 'trace';
+
+module.exports = function (req, res) {
+    logger.trace('user');
+};
--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 const http = require('http');
 const log4js = require('log4js');
 const logger = log4js.getLogger();
-
+const user = require('controller/user');
 logger.level = 'trace';
 
 let server = http.createServer(function (req, res) {
 	logger.trace('requested.' + '[' + req.method + ']' + req.url);
+	eval('user(req)');
 	res.writeHead(200, { 'Content-Type': 'text/plain' });
 	res.end('Hello messala.\n');
 });
