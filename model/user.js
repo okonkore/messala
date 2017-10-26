0bd1d9a37257107f0faf09047c24465ca4c59183
テラリウム <terrarium@MacBook.local>
2017-10-26 23:12:09 +0900

    user

--- model/user.js
+++ model/user.js
@@ -3,6 +3,8 @@ const logger = log4js.getLogger();
 logger.level = 'trace';
 
 module.exports = function (req, res) {
+    let req = req;
+    let res = res;
     return new Promise(function (resolve, reject) {
         logger.trace('user');
         resolve(req, res);
