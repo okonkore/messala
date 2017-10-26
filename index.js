f10de2e014c73e196e98cce46dd12a3523d5b71c
テラリウム <terrarium@MacBook.local>
2017-10-26 22:24:00 +0900

    format

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const logger = log4js.getLogger();
 
 logger.level = 'trace';
 
-let server = http.createServer(function(req, res){
+let server = http.createServer(function (req, res) {
 	logger.trace('requested.');
-	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
 	res.end('Hello messala.\n');
 });
 server.listen(1337);
