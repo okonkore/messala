80fe25e0c746475f5efaa82294fd011b5ea138f7
テラリウム <terrarium@MacBook.local>
2017-10-28 01:37:56 +0900

    controller

--- controller.js
+++ controller.js
@@ -5,8 +5,13 @@ let mappingInfo = {
     // '/mypage': mypage
 };
 module.exports.getModel = function (req, res) {
+    console.log(req.url);
     if (mappingInfo[req.url]) {
         return mappingInfo[req.url](req, res);
+    } else {
+        return new Promise(function (resolve, reject) {
+            resolve('not found.');
+        });
     }
 
 };
