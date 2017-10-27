801ccd5e1f3305791975671b0b3f93bf5b112090
テラリウム <terrarium@MacBook.local>
2017-10-28 02:02:22 +0900

    uuid

--- model/user.js
+++ model/user.js
@@ -2,11 +2,13 @@ const client = require('redis').createClient();
 const log4js = require('log4js');
 const logger = log4js.getLogger();
 logger.level = 'trace';
+const uuidGenerator = require('node-uuid');
 
 module.exports = function (req, res) {
     return new Promise(function (resolve, reject) {
-        client.set('user', 'hoge', function () {
-            resolve('messala user');
+        let uuid = uuidGenerator.v1();
+        client.set('user:' + uuid, uuid, function () {
+            resolve('uuid:' + uuid);
         });
     });
 }
