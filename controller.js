495a9b5b0754d50d3c02f3018f3aee27ef79163c
テラリウム <terrarium@MacBook.local>
2017-10-28 01:35:27 +0900

    user

--- controller.js
+++ controller.js
@@ -1,8 +1,8 @@
 let user = require('./model/user');
 
 let mappingInfo = {
-    '/user': user,
-    '/mypage': mypage
+    '/user': user
+    // '/mypage': mypage
 };
 module.exports.getModel = function (req, res) {
     if (mappingInfo[req.url]) {
