91fe7629b8ee171a3ec1950c67cb2102d704c59a
テラリウム <terrarium@MacBook.local>
2017-10-28 01:53:19 +0900

    redis

--- model/user.js
+++ model/user.js
@@ -1,11 +1,12 @@
+const client = require('redis').createClient();
 const log4js = require('log4js');
 const logger = log4js.getLogger();
 logger.level = 'trace';
 
 module.exports = function (req, res) {
     return new Promise(function (resolve, reject) {
-        setTimeout(function () {
+        client.set('user', 'hoge', function () {
             resolve('messala user');
-        }, 200);
+        });
     });
 }
